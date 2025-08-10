b4d39456e667e5b800316dcd068ab3d4ed6fdec7
Saurabh <saurabhchaurasiya447@gmail.com>
2025-08-10 21:30:52 +0530

    add user account management features: change password, fetch current user, update account details, and manage avatars

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -197,9 +197,114 @@ const refreshAccessToken = asyncHandler(async(req,res)=>{
         throw new apiError(401,error?.message||"invalid refresh token")
     }
 })
+
+const changeCurrentPassword =asyncHandler(async(req,res)=>{
+    const {oldPassword,newPassword}=req.body
+
+    const user=await User.findById(req.user?._id)
+    const isPasswordCorrect=await user.isPasswordCorrect(oldPassword)
+    if(!isPasswordCorrect){
+        throw new apiError(400,"invalid password")
+    }
+    user.password=newPassword;
+    await user.save({validateBeforeSave:false})
+
+
+    return res
+    .status(200)
+    .json(
+        new apiResponce(200,{},"password changed successfully")
+    )
+})
+
+const getCurrentUser =asyncHandler(async(req,res)=>{
+    return res
+    .status(200)
+    .json(200,req.user,"current user fetched successfully")
+})
+
+const updateAccountdetails = asyncHandler(async(req,res)=>{
+    const {fullname,email}=req.body
+    if(!fullname||!email){
+        throw new apiError(400,"all feilds are required")
+    }
+    const user=User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                fullname,
+                email
+            }
+        },
+        {new:true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new apiResponce(200,user,"Account details updated successfully"))
+})
+
+const updateUserAvatar = asyncHandler(async(req,res)=>{
+    const avaterLocalPath=req.file?.path
+    if(!avaterLocalPath){
+        throw new apiError(400,"avatar file is missing")
+    }
+    const avatar=await uploadOnCloudinary(avaterLocalPath)
+    if(!avatar){
+        throw new apiError(400,"error while uploading on cloudinary")
+    }
+    const user=await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                avatar:avatar.url
+            }
+        },
+        {new:true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new apiResponce(200,user,"avatar updated successfully")
+    )
+})
+
+const updateUserCoverImage=asyncHandler(async(req,res)=>{
+    const coverImageLocalPath=req.file?.path
+    if(!coverImageLocalPath){
+        throw new apiError(400,"Cover Image file is missing")
+    }
+    const coverImage=await uploadOnCloudinary(coverImageLocalPath)
+    if(!coverImage){
+        throw new apiError(400,"error while uploading on cloudinary")
+    }
+    const user=await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                coverImage:coverImage.url
+            }
+        },
+        {new:true}
+    ).select("-password")
+    
+    return res
+    .status(200)
+    .json(
+        new apiResponce(200,user,"coverImage updated successfully")
+    )
+    
+})
+
 export {
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountdetails,
+    updateUserAvatar,
+    updateUserCoverImage
 }
--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -4,15 +4,15 @@ const subscriptionSchema = new Schema(
     {
         subscriber:{
             type:Schema.Types.ObjectId,//one who is subscribing
-            ref="User"
+            ref:"User"
         },
         channel:{
             type:Schema.Types.ObjectId,
-            ref="User"
+            ref:"User"
         }
     },
     {timestamps:true}
 )
 
 
-export const Subscription = mongoose.model({"Subscription",subscriptionSchema})
+export const Subscription = mongoose.model({"Subscription":subscriptionSchema})

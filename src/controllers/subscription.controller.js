e71dd8f27070e22803d91d84d87ac7dd12bd9ff2
Saurabh <saurabhchaurasiya447@gmail.com>
2025-10-14 21:25:05 +0530

    feat: implement getUserChannelSubscribers controller with validation and aggregation for subscriber details

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -13,8 +13,40 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
-})
+    const { channelId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(channelId)) {
+        throw new ApiError(400, "Invalid channel ID");
+    }
+
+    const subscribers = await Subscription.aggregate([
+        {
+            $match: {
+                channel: new mongoose.Types.ObjectId(channelId)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",              
+                localField: "subscriber",   
+                foreignField: "_id",        
+                as: "subscriberDetails"  
+            }
+        },
+        {
+            $unwind: "$subscriberDetails"  
+        }
+    ]);
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            subscribers,
+            "Fetched channel subscribers successfully"
+        )
+    );
+});
+
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
